f2bb4e6232b3cc4fa3baf7514c203b77233650e8
do-Od <47896925+do-Od@users.noreply.github.com>
2022-05-05 04:34:19 +0900

    Lv2 Solve_Stringzip

--- src/Coding_Test_Lv1.java
+++ src/Coding_Test_Lv1.java
@@ -7,81 +7,12 @@ public class Coding_Test_Lv1 {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        int[] fees ={1, 461, 1, 10};
-        String[] records={"00:00 1234 IN"};
-        System.out.println(Arrays.toString(solution.solution(fees,records)));
+        String s = "ababcdcdababcdcd";
+        System.out.println(solution.solution(s));
     }
 }
 
 class Solution {
-
-    //lv2_2022_kakao_Blind
-    public int[] solution(int[] fees, String[] records) {
-
-        int base_time = fees[0];
-        int base_price = fees[1];
-        int per_time = fees[2];
-        int per_price = fees[3];
-
-        List<String> in = new ArrayList<>();
-        List<String> out = new ArrayList<>();
-
-        Map<String,Integer> bill = new TreeMap<>();
-
-        for(int i=0;i<records.length;i++){
-            if(records[i].split(" ")[2].equals("IN")) {
-                in.add(records[i]);
-            }else if (records[i].split(" ")[2].equals("OUT")){
-                out.add(records[i]);
-            }
-        }
-
-        for(int i=0;i<in.size();i++){
-            String in_data = in.get(i);
-
-            String carno = in_data.split(" ")[1];
-            String out_data = "";
-
-            for(int j=0;j<out.size();j++){
-                if(out.get(j).split(" ")[1].equals(carno)){
-                    out_data = out.get(j);
-                    out.remove(j);
-                    break;
-                }
-            }
-
-            String out_time = out_data.equals("")?"23:59": out_data.split(" ")[0];
-            String in_time = in_data.split(" ")[0];
-
-            int outt = Integer.valueOf(out_time.split(":")[0])*60 + Integer.valueOf(out_time.split(":")[1]);
-            int intt = Integer.valueOf(in_time.split(":")[0])*60 + Integer.valueOf(in_time.split(":")[1]);
-            int total = outt - intt;
-
-            if(bill.containsKey(carno)) {
-                bill.put(carno,bill.get(carno) + total);
-            }else {
-                bill.put(carno, total);
-            }
-        }
-
-
-        int[] answer = new int[bill.size()];
-        int cnt = 0;
-
-        for(String key : bill.keySet()){
-
-            int time = bill.get(key);
-
-            if(time <= base_time){
-                answer[cnt] = base_price;
-            }else {
-                answer[cnt] =(int)( base_price + Math.ceil((double)(time - base_time) / per_time) * per_price);
-            }
-            cnt++;
-        }
-
-        return answer;
-    }
 
     public String solution(String s) {
 
